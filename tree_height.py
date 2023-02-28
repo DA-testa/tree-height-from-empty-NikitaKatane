e6d29f81d816acf6f638fb07af112abf37869acc
NikitaKatane <125819690+NikitaKatane@users.noreply.github.com>
2023-02-28 12:06:30 +0000

    221RDC055 Nikita Katane

--- tree_height.py
+++ tree_height.py
@@ -1,49 +1,30 @@
-# python3
-
-import sys
-import threading
-import numpy
-
 class Node:
-    def _int_(self,value):
-        self.value =value
-        self.children = []
+    def _init_(self, val):
+        self.val = val
+        self.berni = [] 
 
-def compute_height(n, parents):
-    nodes = [Node(i) for i in range (n)]
-    root = None
-    for i in range(n):
-        if parents[i] == -1
-        root = nodes[i]
+
+def musu_koks(n, vecaki) :
+    nodes = [Node(i) for i in range(n)]
+    sakne = None
+    for i, p in  enumerate(vecaki):
+        if p == -1:
+            sakne = nodes[i]
         else:
-            nodes.[parents].children.append(nodes[i])
-
-
-def dfs(node):
-    if not node.children:
-        return 0 
-        return 1 + max(dfs(child) for child in node.children)
-        
-        return dfs(root)
-        
-def main():
-    n = int(input())
-    parents = list(map(int,input().split()))
-    print(compute_height(n, parents))
-    # implement input form keyboard and from files
+            nodes[p].berni.append(nodes[i])
+    return sakne
     
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
+def dzilums(sakne):
+    if sakne  is None :
+        return 0
+    dzil = 1
+    for mazie in sakne.berni:
+        dzil =  max(dzil, 1 + dzilums(mazie))
+    return dzil 
+    
+
+n = int(input())
+vecaki = list(map(int, input().split()))
+sakne = musu_koks(n, vecaki) 
+print(dzilums(sakne))
